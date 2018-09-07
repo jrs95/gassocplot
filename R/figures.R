80a1b6e526ecec0c106d2a5d690230c7d52240cd
James Staley <j_r_staley@yahoo.co.uk>
2018-09-07 10:53:21 +0100

    Save plots

--- R/figures.R
+++ R/figures.R
@@ -380,7 +380,29 @@ assoc_plot <- function(data, corr, ylab=NULL, title=NULL, subtitle=NULL, type="l
 }
 
 ##########################################################
-##### Assoc plot #####
+##### Save assoc plot #####
+##########################################################
+
+#' assoc_plot_save
+#'
+#' assoc_plot_save saves a png of the assoc_plot with the correct dimensions
+#' @param x the plot
+#' @param file the filepath
+#' @param width the width of the plot
+#' @param height the height of the plot
+#' @import ggplot2 grid gridExtra gtable
+#' @author James R Staley <js16174@bristol.ac.uk>
+#' @export
+assoc_plot_save <- function(x, file, width=NULL, height=NULL){
+  if(is.null(width)){width <- 800}
+  if(is.null(height)){height <- 800}
+  png(file, width=width, height=height)
+  grid.draw(x)
+  dev.off()
+}
+
+##########################################################
+##### Stack assoc plot #####
 ##########################################################
 
 #' plot_assoc_stack
@@ -624,3 +646,23 @@ stack_assoc_plot <- function(markers, z, corr, traits, x.min=NULL, x.max=NULL, t
   return(combined.plot)
 }
 
+##########################################################
+##### Save assoc plot #####
+##########################################################
+
+#' stack_assoc_plot_save
+#'
+#' stack_assoc_plot_save saves a png of the assoc_plot with the correct dimensions
+#' @param x the plot
+#' @param file the filepath
+#' @param n_traits the filepath
+#' @import ggplot2 grid gridExtra gtable
+#' @author James R Staley <js16174@bristol.ac.uk>
+#' @export
+stack_assoc_plot_save <- function(x, file, n_traits, width=NULL, height=NULL){
+  if(is.null(width)){width <- 800}
+  if(is.null(height)){height <- 800 + 400*(n_traits-1)}  
+  png(file, width=width, height=height)
+  grid.draw(x)
+  dev.off()
+}
